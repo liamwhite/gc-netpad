496ec0b36dda4c792f1b731edef76d2a6eae67fb
Liam P. White <inkscapebrony@gmail.com>
2014-11-23 21:17:01 -0500

    Initial commit

---
+++ client/client.c
@@ -0,0 +1,191 @@
+#include <arpa/inet.h>
+#include <byteswap.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <linux/uinput.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Virtual stick radius used by uinput.
+static const int kStickRadius = 128;
+
+// Struct holding all data required for a complete controller stat
+struct _padstat_pack {
+    short buttonsheld, buttonsdown, buttonsup;
+    char stick1X, stick1Y, stick2X, stick2Y;
+} __attribute__((packed));
+
+// Swap the bytes of the shorts to make up for the different endian-ness of the PPC vs Intel
+static inline void _endian_byteswap(struct _padstat_pack *in) {
+    in->buttonsheld = bswap_16(in->buttonsheld);
+    in->buttonsdown = bswap_16(in->buttonsdown);
+    in->buttonsup   = bswap_16(in->buttonsup);
+}
+
+static int _net_init(void);
+static void _pad_data_to_uinput(struct _padstat_pack const *in);
+
+// Socket descriptor
+static int desc;
+
+int main(void)
+{
+    struct _padstat_pack pad = {0};
+
+    if (_net_init() != 0) return 1;
+
+    printf("Net initialized\n");
+
+    // Receieve data
+    while (1) {
+        recv(desc, &pad, sizeof(struct _padstat_pack), 0);
+        _endian_byteswap(&pad);
+        _pad_data_to_uinput(&pad);
+    }
+}
+
+static int _net_init(void)
+{
+    struct sockaddr_in sock = {0};
+
+    char const *wii = getenv("WII");
+    if (!wii || *wii == '\0') {
+        printf("Failed to get Wii's IP address from shell env WII");
+        return -1;
+    }
+
+    // Open socket for TCP communication
+    desc = socket(AF_INET, SOCK_STREAM, 6);
+
+    if (desc == -1) {
+    
+        printf("Failed to open socket\n");
+        return -1;
+    }
+
+    // Connect socket
+    sock.sin_family = AF_INET;
+    sock.sin_port = htons(80);
+    inet_pton(AF_INET, wii, &sock.sin_addr);
+
+    return connect(desc, (struct sockaddr *) &sock, sizeof(struct sockaddr_in));
+}
+
+// Shamelessly copied from libdrc.
+
+static int _init_uinput(void)
+{
+    // Try to open the uinput file descriptor.
+    /*const char* uinput_filename[] = {
+        "/dev/uinput",
+        "/dev/input/uinput",
+        "/dev/misc/uinput"
+    };
+
+    int uinput_fd;
+    for (size_t i = 0; i < 3; ++i) {
+        if ((uinput_fd = open(uinput_filename[i], O_RDWR)) >= 0) {
+            break;
+        }
+    }*/
+    int uinput_fd = open("/dev/uinput", O_RDWR);
+
+    if (uinput_fd < 0) {
+        return -1;
+    }
+
+    // Create the virtual device.
+    struct uinput_user_dev dev = {0};
+    strncpy(dev.name, "NetGamecube Controller", sizeof(dev.name));
+    dev.id.bustype = BUS_VIRTUAL;
+
+    // Enable joysticks: 4 axes (2 for each stick).
+    for (int i = 0; i < 4; ++i) {
+        dev.absmin[i] = -kStickRadius;
+        dev.absmax[i] = kStickRadius;
+        ioctl(uinput_fd, UI_SET_ABSBIT, i);
+    }
+
+    // Enable 16 buttons (we only use 12 of these, but it makes things easier).
+    for (int i = 0; i < 16; ++i) {
+        ioctl(uinput_fd, UI_SET_KEYBIT, BTN_JOYSTICK + i);
+    }
+
+    // Set the absolute and key bits.
+    ioctl(uinput_fd, UI_SET_EVBIT, EV_ABS);
+    ioctl(uinput_fd, UI_SET_EVBIT, EV_KEY);
+
+    // Create the device.
+    if (write(uinput_fd, &dev, sizeof (dev)) != sizeof (dev)) {
+        perror("write failed - creating the device");
+    }
+
+    ioctl(uinput_fd, UI_DEV_CREATE);
+
+    return uinput_fd;
+}
+
+static void _pad_data_to_uinput(struct _padstat_pack const *in)
+{
+    static _Bool initialized = 0;
+    static int uinput_fd = -1;
+    if (!initialized) {
+        initialized = 1;
+        uinput_fd = _init_uinput();
+        if (uinput_fd == -1) {
+            perror("uinput feeder");
+        }
+    }
+
+    if (uinput_fd == -1) {
+        return;
+    }
+
+    // Get the current timestamp
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    // 16 button events + 4 axis events + 1 report event.
+    struct input_event evt[16 + 4 + 1];
+    memset(&evt, 0, sizeof(evt));
+
+    // Read the 16 buttons and initialize their event object.
+    for (int i = 0; i < 16; ++i) {
+        evt[i].type = EV_KEY;
+        evt[i].code = BTN_JOYSTICK + i;
+        evt[i].value = !!(in->buttonsheld & (1 << i));
+        memcpy(&evt[i].time, &tv, sizeof(tv));
+    }
+
+    // Read the 4 axis values.
+    short sticks[] = {
+        in->stick1X, in->stick1Y,
+        in->stick2X, in->stick2Y
+    };
+
+    for (int i = 16; i < 16 + 4; ++i) {
+        evt[i].type = EV_ABS;
+        evt[i].code = i - 16;
+        evt[i].value = sticks[i - 16]; /*((int)(sticks[i - 16] * kStickRadius));*/
+        memcpy(&evt[i].time, &tv, sizeof(tv));
+    }
+
+    // Report event.
+    evt[20].type = EV_SYN;
+    evt[20].code = SYN_REPORT;
+    memcpy(&evt[20].time, &tv, sizeof(tv));
+
+    // Send the events to uinput.
+    for (size_t i = 0; i < 21; ++i) {
+        if (write(uinput_fd, &evt[i], sizeof(evt[i])) != sizeof(evt[i])) {
+            perror("write failed - sending an event to uinput");
+        }
+    }
+}
---
+++ server/source/server.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <malloc.h>
+#include <ogcsys.h>
+#include <gccore.h>
+#include <network.h>
+#include <debug.h>
+#include <errno.h>
+
+static void *xfb = NULL;
+static GXRModeObj *rmode = NULL;
+
+static s32 sock, csock;
+
+void *initialise();
+static int _net_init(void);
+
+struct _padstat_pack {
+    short buttonsheld, buttonsdown, buttonsup;
+    char stick1X, stick1Y, stick2X, stick2Y;
+} __attribute__((packed));
+
+// @pre PAD_Init() was already called
+static void _get_pad_data(struct _padstat_pack *in, int pad)
+{
+    PAD_ScanPads();
+
+    in->buttonsheld = PAD_ButtonsHeld(pad);
+    in->buttonsdown = PAD_ButtonsDown(pad);
+    in->buttonsup   = PAD_ButtonsUp(pad);
+    in->stick1X     = (char)PAD_StickX(pad);
+    in->stick1Y     = (char)PAD_StickY(pad);
+    in->stick2X     = (char)PAD_SubStickX(pad);
+    in->stick2Y     = (char)PAD_SubStickY(pad);
+}
+
+int main(int argc, char **argv)
+{
+    struct _padstat_pack pad1 = {0};
+    struct _padstat_pack pad_tmp = {0};
+
+    xfb = initialise();
+
+    if (_net_init() != 0) return 1;
+
+    // Now we've trapped our client, start sending our payload
+    while (1) {
+        _get_pad_data(&pad_tmp, 0);
+
+        // Pad data are different
+        if (memcmp(&pad1, &pad_tmp, sizeof(struct _padstat_pack)) != 0) {
+            // Copy it over
+            memcpy(&pad1, &pad_tmp, sizeof(struct _padstat_pack));
+            // Fire
+            net_send(csock, &pad1, sizeof(struct _padstat_pack), 0);
+        }
+    }
+
+    net_close(csock);
+
+    return 0;
+}
+
+static int _net_init(void)
+{
+    struct sockaddr_in client;
+    struct sockaddr_in server;
+
+    char localip[16] = {0};
+    char gateway[16] = {0};
+    char netmask[16] = {0};
+    int bind = 0;
+
+    u32 clientlen;
+
+    // Configure the network interface
+    bind = if_config (localip, netmask, gateway, TRUE);
+    if (bind >= 0) {
+        printf("network configured, ip: %s, gw: %s, mask %s\n", localip, gateway, netmask);
+        VIDEO_WaitVSync();
+    } else {
+        printf("network configuration failed!\n");
+        return 1;
+    }
+
+    clientlen = sizeof(client);
+    sock = net_socket (AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (sock == INVALID_SOCKET) {
+        printf("Cannot create a socket!\n");
+        return 1;
+    }
+
+    memset (&server, 0, sizeof (server));
+    memset (&client, 0, sizeof (client));
+
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+    server.sin_addr.s_addr = INADDR_ANY;
+    bind = net_bind (sock, (struct sockaddr *) &server, sizeof(server));
+    if (bind) {
+        printf("Error %d binding socket!\n", bind);
+        return 1;
+    }
+    if ((bind = net_listen(sock, 5))) {
+        printf("Error %d listening!\n", bind);
+        return 1;
+    }
+    csock = net_accept(sock, (struct sockaddr *) &client, &clientlen);
+    if (csock < 0) {
+        printf("Error connecting socket %d!\n", csock);
+        return 1;
+    }
+
+    // return execution to main
+    return 0;
+}
+
+void *initialise()
+{
+    void *framebuffer;
+
+    VIDEO_Init();
+    PAD_Init();
+
+    rmode = VIDEO_GetPreferredMode(NULL);
+    framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+    console_init(framebuffer,20,20,rmode->fbWidth,rmode->xfbHeight,rmode->fbWidth*VI_DISPLAY_PIX_SZ);
+
+    VIDEO_Configure(rmode);
+    VIDEO_SetNextFramebuffer(framebuffer);
+    VIDEO_SetBlack(FALSE);
+    VIDEO_Flush();
+    VIDEO_WaitVSync();
+    if(rmode->viTVMode&VI_NON_INTERLACE) VIDEO_WaitVSync();
+
+    return framebuffer;
+}
